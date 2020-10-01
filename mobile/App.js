0b9ae42deec8c1ed60a6f7a46e8a93242a8cc900
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-10-01 14:50:21 +0300

    basic navigation added

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Navigation from './src/routes/Navigation';
 
 const App = () => {
   return (
-    <View>
-      <Text>App screen </Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
 
---
+++ mobile/src/routes/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/Home';
+import Camera from '../screens/Camera';
+import Messenger from '../screens/Messenger';
+import Profile from '../screens/Profile';
+import Settings from '../screens/Settings';
+import {Button} from 'react-native';
+
+const Stack = createStackNavigator();
+
+function Navigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Camera" component={Camera} />
+      <Stack.Screen name="Messenger" component={Messenger} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+}
+
+export default Navigation;
