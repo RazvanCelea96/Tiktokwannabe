a8b28595a196bb9a983e26e396d71b966ae52b06
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-10-04 19:49:06 +0300

    Done profile navigation backbone

--- mobile/src/screens/Home/HomeComments.js
+++ mobile/src/screens/Home/HomeComments.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function HomeComments() {
+export default function HomeComments({navigation}) {
   return (
     <View>
       <Text>Home comments screen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Settings')}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Go to Settings</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: '#009688',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginTop: 25,
+    marginLeft: 50,
+    marginRight: 50,
+  },
+
+  appButtonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+});
--- mobile/src/screens/Home/Profile/Profile.js
+++ mobile/src/screens/Home/Profile/Profile.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function Profile() {
+export default function Profile({navigation}) {
   return (
     <View>
       <Text>Profile screen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Following')}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Go to Following</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Followers')}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Go to Followers</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('StoryViews')}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Go to Story views</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Settings')}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Go to Settings</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: '#009688',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginTop: 25,
+    marginLeft: 50,
+    marginRight: 50,
+  },
+
+  appButtonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+});
