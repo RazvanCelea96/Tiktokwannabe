a3e0964917686f16ddd59b8fc867f66d24e77c08
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-10-11 20:08:57 +0300

    Finished Caption Edit screen

--- mobile/src/screens/Home/Profile/Settings/CaptionEdit.js
+++ mobile/src/screens/Home/Profile/Settings/CaptionEdit.js
@@ -1,10 +1,74 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 
-export default function CaptionEdit() {
+export default function CaptionEdit({navigate}) {
   return (
-    <View>
-      <Text>Caption Edit screen</Text>
+    <View
+      style={{
+        padding: 10,
+        paddingTop: 60,
+        height: '100%',
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        backgroundColor: '#fff',
+        paddingLeft: 20,
+      }}>
+      <View style={{marginBottom: 10}}>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: '500',
+            alignItems: 'center',
+            lineHeight: 21,
+            color: 'black',
+          }}>
+          Type in your new caption down below. When your done, press save!
+        </Text>
+      </View>
+
+      <View
+        style={{
+          width: 335,
+          height: 150,
+          justifyContent: 'center',
+          paddingHorizontal: 15,
+          marginTop: 50,
+        }}>
+        <TextInput
+          style={{
+            width: 335,
+            color: 'black',
+            borderRadius: 20,
+            backgroundColor: '#f4f6f8',
+            paddingBottom: 100,
+          }}
+          placeholder="Type a caption..."
+          autoCorrect={false}
+          autoCompleteType="off"
+          keyboardAppearance="dark"
+          numberOfLines={1}
+          spellCheck={false}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={{
+          width: 169,
+          backgroundColor: '#FF3A72',
+          borderRadius: 100,
+          height: 56,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 50,
+          marginLeft: 100,
+        }}
+        onPress={() => {
+          navigation.navigate('Settings');
+        }}>
+        <Text style={{color: 'white', fontSize: 17}}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 }
